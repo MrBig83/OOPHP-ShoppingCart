d54fa325e59464022b6cee5c477a637611875208
MrBig83 <mjohansson176@gmail.com>
2022-11-30 23:26:48 +0100

    G klart. Jobbar på VG

--- CartItem.php
+++ CartItem.php
@@ -3,17 +3,32 @@
 
 class CartItem
 {
+    public function __construct($product, $quantity){
+        $this->product = $product;
+        $this->quantity = $quantity;
+    }
+
     private $product;
-    private int $quantity;
+    private $quantity;
 
-    // TODO Skriv en konstruktor som sätter alla properties
+    // TODO Skriv en konstruktor som sätter alla properties == DONE
 
-    // TODO Skriv getters för alla properties
+    // TODO Skriv getters för alla properties == DONE
 
+    public function getProduct(){
+        return $this->product;
+    }
+    public function getQuantity(){
+        return $this->quantity;
+    }
 
     //VG: Skall utöka antalet på ett cartItem med 1. 
     //VG: Det skall inte vara möjligt att utöka så att antalet överstiger produktens $inStock.
     public function increaseQuantity()
     {
+        
+        if($this -> quantity < $this -> product -> getInStock()){
+            $this -> quantity += 1;
+        }
     }
 }
--- Product.php
+++ Product.php
@@ -1,15 +1,35 @@
 <?php
 
-
 class Product
 {
+
+    public function __construct($id, $title, $price, $inStock){
+        $this->id = $id;
+        $this->title = $title;
+        $this->price = $price;
+        $this->inStock = $inStock;
+    }
+
     private $id;
     private $title;
     private $price;
     private $inStock;
 
-    // TODO Skriv en konstruktor som sätter alla properties
+    // TODO Skriv en konstruktor som sätter alla properties == DONE
 
-    // TODO Skriv getters för alla properties
+    // TODO Skriv getters för alla properties == DONE
+
+    public function getId(){
+        return $this->id;
+    }
+    public function getTitle(){
+        return $this->title;
+    }
+    public function getPrice(){
+        return $this->price;
+    }
+    public function getInStock(){
+        return $this->inStock;
+    }
 
 }
